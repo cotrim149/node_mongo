2a0ef0c885c45e04d48628336df56413aaee85e7
Victor Cotrim <cotrim149@gmail.com>
2018-09-09 10:33:44 -0300

    Using destructuring to get MongoDB and ObjectId

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,13 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+// using destructuring to get MongoClient
+const {MongoClient, ObjectId} = require('mongodb');
+
+const user = {name: 'Cotrim', age: 28};
+// Get the parameter name from JSON, ES6 destructuring
+var {name} = user;
+console.log(name);
+
+
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, client) => {
   if (err) {
@@ -21,16 +30,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, client) => {
 
   // });
 
-  db.collection('Users').insertOne({
-    name: 'Cotrim',
-    age: 28,
-    location: 'Vicente pires'
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert user', err);
-    }
-    console.log(JSON.stringify(result.ops,undefined,2));
-  });
+  // db.collection('Users').insertOne({
+  //   name: 'Cotrim',
+  //   age: 28,
+  //   location: 'Vicente pires'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops,undefined,2));
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
 
   client.close();
